fce67158ead92122c9168f72b7019c49d7b93cde
Aviral Malik <82748379+floofy244@users.noreply.github.com>
2025-01-09 00:02:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5001";
+const API_URL = "https://ai-chat-backend-itoi.onrender.com/";
 
 async function uploadFile() {
   const fileInput = document.getElementById("fileInput");
